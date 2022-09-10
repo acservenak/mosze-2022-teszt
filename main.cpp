464a7a273e1a3696bc3123970f87d648afb0d5f3
acservenak <o.attila.cservenak@gmail.com>
2022-09-10 12:32:40 +0200

    fixed errors

--- main.cpp
+++ main.cpp
@@ -5,29 +5,29 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {   
    
-    int *b = new int[NELEMENTS];  //identifier "NELEMENTS" is undefined - hiányzó '_'
+    int *b = new int[N_ELEMENTS];  
  
     // a ' ' nem megfelelő -> csere: " "
     //olvashatóság miatt endl hiányzik
-    std::cout << '1-100 ertekek duplazasa' //expected a ';' - hiányzó ';' a sor végén
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //expected a ';' - hiányzó ';' a sor végén
 
 
-    for (int i = 0;) //Nem megfelelő iteráció, hiányzik a feltétel vizsgálat és a lépésenként végrehajtandó számláló növelése
+    for (int i = 0; i < N_ELEMENTS; i++) //Nem megfelelő iteráció, hiányzik a feltétel vizsgálat és a lépésenként végrehajtandó számláló növelése
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //Nem megfelelő feltétel vizsgálat - esetünkben: i < N_ELEMENTS
+    for (int i = 0; i < N_ELEMENTS; i++) //Nem megfelelő feltétel vizsgálat - esetünkben: i < N_ELEMENTS
     {
         // Az értékek nincsenek kiiratva
         //Hiányzó endl az olvashatóság miatt
-        std::cout << "Ertek:" //expected a ';' - hiányzó ';' a sor végén
+        std::cout << "Ertek: " << b[i] << std::endl; //expected a ';' - hiányzó ';' a sor végén
     }
 
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //Átlagszámítás: double
-    for (int i = 0; i < N_ELEMENTS, i++) //expected a ';' - hiányzó ';' az N_ELEMENTS után, a ',' nem megfelelő 
+    double atlag = 0; //Átlagszámítás: double
+    for (int i = 0; i < N_ELEMENTS; i++) //expected a ';' - hiányzó ';' az N_ELEMENTS után, a ',' nem megfelelő 
     {
-        atlag += b[i] //expected a ';' - hiányzó ';' a sor végén
+        atlag += b[i]; //expected a ';' - hiányzó ';' a sor végén
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
